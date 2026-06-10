3300a112a22959bf63d4575d9ab3826898a1e09b
2019Milton <55396612+2019Milton@users.noreply.github.com>
2026-06-10 18:49:19 -0300

    Reverte permissao instagram_basic (escopo invalido para o app, bloqueava login Meta)

--- index.ts
+++ index.ts
@@ -3007,8 +3007,7 @@ app.get("/auth/meta/login", async (c) => {
     "leads_retrieval",
     "pages_show_list",
     "pages_read_engagement",
-    "pages_manage_ads",
-    "instagram_basic"
+    "pages_manage_ads"
 
   ].join(",");
 
