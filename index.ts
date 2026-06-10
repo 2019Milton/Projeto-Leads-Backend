94b2b03e9afd122db34b24ac20c4171a5c7a9ccc
2019Milton <55396612+2019Milton@users.noreply.github.com>
2026-06-10 12:14:07 -0300

    Adiciona instagram_business_basic ao escopo OAuth (permissao moderna liberada no app)

--- index.ts
+++ index.ts
@@ -2984,7 +2984,8 @@ app.get("/auth/meta/login", async (c) => {
     "leads_retrieval",
     "pages_show_list",
     "pages_read_engagement",
-    "pages_manage_ads"
+    "pages_manage_ads",
+    "instagram_business_basic"
 
   ].join(",");
 
